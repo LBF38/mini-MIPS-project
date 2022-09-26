46814aba1326a19b4e83ae81303ec9973243e47f
LBF38 <mathis.urien@free.fr>
2022-09-26 10:52:53 +0200

    Implementing the LC-3 VM

--- lc3.c
+++ lc3.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <signal.h>
-// /* unix only */
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <sys/time.h>
-// #include <sys/types.h>
-// #include <sys/termios.h>
-// #include <sys/mman.h>
+/* unix only */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 // Memory Storage
 #define MEMORY_MAX (1 << 16)
-uint16_t memory[MEMORY_MAX];  /* 65536 locations */
+uint16_t memory[MEMORY_MAX]; /* 65536 locations */
 
 // Registers
 enum
@@ -62,8 +62,137 @@ enum
     OP_TRAP    /* execute trap */
 };
 
+// Sign Extend
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1)
+    {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
 
+// Update Flags
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
 
+// Main Loop
+int main(int argc, const char *argv[])
+{
+    // Load Arguments
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+    // for (int j = 1; j < argc; ++j)
+    // {
+    //     if (!read_image(argv[j]))
+    //     {
+    //         printf("failed to load image: %s\n", argv[j]);
+    //         exit(1);
+    //     }
+    // }
 
+    // Setup
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
 
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum
+    {
+        PC_START = 0x3000
+    };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+        case OP_ADD:
+            /* destination register (DR) */
+            uint16_t r0 = (instr >> 9) & 0x7;
+            /* first operand (SR1) */
+            uint16_t r1 = (instr >> 6) & 0x7;
+            /* whether we are in immediate mode */
+            uint16_t imm_flag = (instr >> 5) & 0x1;
+
+            if (imm_flag)
+            {
+                uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                reg[r0] = reg[r1] + imm5;
+            }
+            else
+            {
+                uint16_t r2 = instr & 0x7;
+                reg[r0] = reg[r1] + reg[r2];
+            }
+
+            update_flags(r0);
+            break;
+        case OP_AND:
+            // A implémenter
+        case OP_NOT:
+            // A implémenter
+        case OP_BR:
+            // A implémenter
+        case OP_JMP:
+            // A implémenter
+        case OP_JSR:
+            // A implémenter
+        case OP_LD:
+            // A implémenter
+        case OP_LDI:
+            /* destination register (DR) */
+            uint16_t r0 = (instr >> 9) & 0x7;
+            /* PCoffset 9*/
+            uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+            /* add pc_offset to the current PC, look at that memory location to get the final address */
+            reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+            update_flags(r0);
+            break;
+        case OP_LDR:
+            // A implémenter
+        case OP_LEA:
+            // A implémenter
+        case OP_ST:
+            // A implémenter
+        case OP_STI:
+            // A implémenter
+        case OP_STR:
+            // A implémenter
+        case OP_TRAP:
+            // A implémenter
+        case OP_RES:
+        case OP_RTI:
+        default:
+            abort();
+            break;
+        }
+    }
+    // Shutdown
+    restore_input_buffering();
+}
--- makefile
+++ makefile
@@ -1,6 +1,7 @@
+default: vm lc3
 vm: vm.c
 	gcc -Wall vm.c -o vm
 	./vm
 lc3: lc3.c
-	gcc -Wall lc3.c -o lc3
-	./lc3
+	gcc -Wall lc3.c -o lc3-vm
+	./lc3-vm
