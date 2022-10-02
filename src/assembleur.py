f59c260edb460ffec8405020db70f5f21634302e
LBF38 <mathis.urien@free.fr>
2022-10-02 23:47:10 +0200

    Création fichier pour assembleur en python

---
+++ src/assembleur.py
@@ -0,0 +1,35 @@
+## Ici, on définit les règles pour convertir les commandes assembleur en binaire.
+# Utilisation de Regex pour vérifier les syntaxes par exemple.
+# Lecture de fichiers, vérification des expressions, convertir en binaire selon règles binaires.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
