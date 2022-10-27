2212b789d7cd8f973633345eec07fc7a52fe3700
LBF38 <mathis.urien@free.fr>
2022-10-27 15:56:35 +0200

    Added recognition for every operators in the VM
    The VM can recognize every operators !!
    And the code is working correctly !

--- src/vm.c
+++ src/vm.c
@@ -6,9 +6,6 @@
 #include "vm.h"
 
 u_int32_t memory[MEMORY_SIZE];
-// u_int32_t PC = 0; // program counter
-// u_int32_t IR;     // instruction register
-
 unsigned registers[NUM_REGS];
 
 /* program counter */
@@ -16,10 +13,16 @@ int pc = 0;
 /* instruction fields */
 int instruction;
 int opcode = 0;
-int reg1 = 0;
-int reg2 = 0;
-int reg3 = 0;
-int imm = 0;
+int rd = 0;
+int rs = 0;
+int rs1 = 0;
+int rs2 = 0;
+int ra = 0;
+int addr = 0;
+int n = 0;
+int immediate = 0;
+/* the VM runs until this flag becomes 0 */
+int running = 1;
 
 void print_memory()
 {
@@ -54,88 +57,318 @@ void read_file(char *filename)
     }
 }
 
-/* decode a word */
-void decode_i(int instruction)
+void decode_r()
 {
-    opcode = (instruction & 0xF000) >> 12;
-    reg1 = (instruction & 0xF00) >> 8;
-    reg2 = (instruction & 0xF0) >> 4;
-    reg3 = (instruction & 0xF);
-    imm = (instruction & 0xFF);
+    rd = (instruction >> 21) & 0x1f;
+    rs1 = (instruction >> 16) & 0x1f;
+    rs2 = (instruction >> 11) & 0x1f;
+}
+void decode_i()
+{
+    rd = (instruction >> 21) & 0x1f;
+    rs = (instruction >> 16) & 0x1f;
+    immediate = instruction & 0xff;
+}
+void decode_jr()
+{
+    rd = (instruction >> 21) & 0x1f;
+    ra = (instruction >> 16) & 0x1f;
+}
+void decode_ji()
+{
+    rd = (instruction >> 21) & 0x1f;
+    addr = instruction & 0x1fffff;
+}
+void decode_b()
+{
+    rs = (instruction >> 21) & 0x1f;
+    addr = instruction & 0x1fffff;
+}
+void decode_s()
+{
+    n = instruction & 0x3ffffff;
 }
 
-void add()
+void op_add()
 {
-    int rd = (instruction >> 21) & 0x1f;
-    int rs1 = (instruction >> 16) & 0x1f;
-    int rs2 = (instruction >> 11) & 0x1f;
+    decode_r();
     printf("add r%d r%d r%d\n", rd, rs1, rs2);
 }
 
-void addi()
+void op_addi()
 {
-    int rd = (instruction >> 21) & 0x1f;
-    int rs = (instruction >> 16) & 0x1f;
-    int immediate = (instruction & 0xFF);
+    decode_i();
     printf("add r%d r%d %d\n", rd, rs, immediate);
 }
 
-/* the VM runs until this flag becomes 0 */
-int running = 1;
+void op_sub()
+{
+    decode_r();
+    printf("sub r%d r%d r%d\n", rd, rs1, rs2);
+    // registers[reg1] = registers[reg2] - registers[reg3];
+}
+void op_subi()
+{
+    decode_i();
+    printf("sub r%d r%d %d\n", rd, rs, immediate);
+}
+void op_mul()
+{
+    decode_r();
+    printf("mul r%d r%d r%d\n", rd, rs1, rs2);
+    // registers[reg1] = registers[reg2] * registers[reg3];
+}
+void op_muli()
+{
+    decode_i();
+    printf("mul r%d r%d %d\n", rd, rs, immediate);
+}
+void op_div()
+{
+    decode_r();
+    printf("div r%d r%d r%d\n", rd, rs1, rs2);
+    // if (registers[reg3] == 0)
+    // {
+    //     fprintf(stderr, "error input: division by zero\n");
+    //     exit(-1);
+    // }
+    // registers[reg1] = registers[reg2] / registers[reg3];
+}
+void op_divi()
+{
+    decode_i();
+    printf("div r%d r%d %d\n", rd, rs, immediate);
+}
+void op_and()
+{
+    decode_r();
+    printf("and r%d r%d r%d\n", rd, rs1, rs2);
+    // registers[reg1] = registers[reg2] && registers[reg3];
+}
+void op_andi()
+{
+    decode_i();
+    printf("and r%d r%d %d\n", rd, rs, immediate);
+}
+void op_or()
+{
+    decode_r();
+    printf("or r%d r%d r%d\n", rd, rs1, rs2);
+    // registers[reg1] = registers[reg2] || registers[reg3];
+}
+void op_ori()
+{
+    decode_i();
+    printf("or r%d r%d %d\n", rd, rs, immediate);
+}
+void op_xor()
+{
+    decode_r();
+    printf("xor r%d r%d r%d\n", rd, rs1, rs2);
+    // registers[reg1] = registers[reg2] ^ registers[reg3];
+}
+void op_xori()
+{
+    decode_i();
+    printf("xor r%d r%d %d\n", rd, rs, immediate);
+}
+void op_shl()
+{
+    decode_r();
+    printf("shl r%d r%d r%d\n", rd, rs1, rs2);
+}
+void op_shli()
+{
+    decode_i();
+    printf("shl r%d r%d %d\n", rd, rs, immediate);
+}
+void op_shr()
+{
+    decode_r();
+    printf("shr r%d r%d r%d\n", rd, rs1, rs2);
+}
+void op_shri()
+{
+    decode_i();
+    printf("shr r%d r%d %d\n", rd, rs, immediate);
+}
+void op_slt()
+{
+    decode_r();
+    printf("slt r%d r%d r%d\n", rd, rs1, rs2);
+}
+void op_slti()
+{
+    decode_i();
+    printf("slt r%d r%d %d\n", rd, rs, immediate);
+}
+void op_sle()
+{
+    decode_r();
+    printf("sle r%d r%d r%d\n", rd, rs1, rs2);
+}
+void op_slei()
+{
+    decode_i();
+    printf("sle r%d r%d %d\n", rd, rs, immediate);
+}
+void op_seq()
+{
+    decode_r();
+    printf("seq r%d r%d r%d\n", rd, rs1, rs2);
+}
+void op_seqi()
+{
+    decode_i();
+    printf("seq r%d r%d %d\n", rd, rs, immediate);
+}
+void op_load()
+{
+    // A vérifier
+    decode_i();
+    printf("load r%d r%d %d\n", rd, rs, immediate);
+    // registers[reg1] = imm;
+}
+void op_store()
+{
+    decode_i();
+    printf("store r%d r%d %d\n", rd, rs, immediate);
+}
+void op_jmpr()
+{
+    decode_jr();
+    printf("jmp r%d r%d\n", rd, ra);
+}
+void op_jmpi()
+{
+    decode_ji();
+    printf("jmp r%d %d\n", rd, addr);
+}
+void op_braz()
+{
+    decode_b();
+    printf("braz r%d %d\n", rs, addr);
+}
+void op_branz()
+{
+    decode_b();
+    printf("branz r%d %d\n", rs, addr);
+}
+void op_scall()
+{
+    decode_s();
+    printf("scall %d\n", n);
+}
+void op_stop()
+{
+    printf("stop\n");
+    running = 0;
+}
 
 /* evaluate the last decoded instruction */
 void eval()
 {
     switch (opcode)
     {
-    case OPCODE_STOP:
-        printf("Stop\n");
-        running = 0;
-        break;
-    case OPCODE_LOAD:
-        printf("load r%d #%d\n", reg1, imm);
-        registers[reg1] = imm;
-        break;
     case OPCODE_ADD:
-        add();
-        // printf("add r%d r%d r%d\n", reg1, reg2, reg3);
-        // registers[reg1] = registers[reg2] + registers[reg3];
+        op_add();
         break;
     case OPCODE_ADDI:
-        addi();
+        op_addi();
         break;
     case OPCODE_SUB:
-        printf("sub r%d r%d r%d\n", reg1, reg2, reg3);
-        registers[reg1] = registers[reg2] - registers[reg3];
+        op_sub();
+        break;
+    case OPCODE_SUBI:
+        op_subi();
         break;
     case OPCODE_MUL:
-        printf("mult r%d r%d r%d\n", reg1, reg2, reg3);
-        registers[reg1] = registers[reg2] * registers[reg3];
+        op_mul();
+        break;
+    case OPCODE_MULI:
+        op_muli();
         break;
     case OPCODE_DIV:
-        printf("div r%d r%d r%d\n", reg1, reg2, reg3);
-        if (registers[reg3] == 0)
-        {
-            fprintf(stderr, "error input: division by zero\n");
-            exit(-1);
-        }
-        registers[reg1] = registers[reg2] / registers[reg3];
+        op_div();
+        break;
+    case OPCODE_DIVI:
+        op_divi();
         break;
     case OPCODE_AND:
-        printf("and r%d r%d r%d\n", reg1, reg2, reg3);
-        registers[reg1] = registers[reg2] && registers[reg3];
+        op_and();
+        break;
+    case OPCODE_ANDI:
+        op_andi();
         break;
     case OPCODE_OR:
-        printf("or r%d r%d r%d\n", reg1, reg2, reg3);
-        registers[reg1] = registers[reg2] || registers[reg3];
+        op_or();
+        break;
+    case OPCODE_ORI:
+        op_ori();
         break;
     case OPCODE_XOR:
-        printf("xor r%d r%d r%d\n", reg1, reg2, reg3);
-        registers[reg1] = registers[reg2] ^ registers[reg3];
+        op_xor();
+        break;
+    case OPCODE_XORI:
+        op_xori();
+        break;
+    case OPCODE_SHL:
+        op_shl();
+        break;
+    case OPCODE_SHLI:
+        op_shli();
+        break;
+    case OPCODE_SHR:
+        op_shr();
+        break;
+    case OPCODE_SHRI:
+        op_shri();
+        break;
+    case OPCODE_SLT:
+        op_slt();
+        break;
+    case OPCODE_SLTI:
+        op_slti();
+        break;
+    case OPCODE_SLE:
+        op_sle();
+        break;
+    case OPCODE_SLEI:
+        op_slei();
+        break;
+    case OPCODE_SEQ:
+        op_seq();
+        break;
+    case OPCODE_SEQI:
+        op_seqi();
+        break;
+    case OPCODE_LOAD:
+        op_load();
+        break;
+    case OPCODE_STORE:
+        op_store();
+        break;
+    case OPCODE_JMPR:
+        op_jmpr();
+        break;
+    case OPCODE_JMPI:
+        op_jmpi();
+        break;
+    case OPCODE_BRAZ:
+        op_braz();
+        break;
+    case OPCODE_BRANZ:
+        op_branz();
+        break;
+    case OPCODE_SCALL:
+        op_scall();
+        break;
+    case OPCODE_STOP:
+        op_stop();
         break;
     default:
         perror("ERROR: unknown opcode\n");
-        printf("Opcode : %d\n",opcode);
+        printf("Opcode : %d\n", opcode);
         running = 0;
         break;
     }
@@ -164,13 +397,13 @@ void run()
 {
     while (running)
     {
-        showRegs();
+        // showRegs();
         instruction = memory[pc];
         opcode = (instruction >> 26) & 0x3f;
         eval();
         pc++;
     }
-    showRegs();
+    // showRegs();
 }
 
 int main(int argc, const char *argv[])
